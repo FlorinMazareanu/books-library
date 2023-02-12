8ccd332e102e20cb2a1bfc9fd50423f52ba778dc
FlorinMazareanu <florin.mazareanu@gmail.com>
2023-02-12 16:34:38 +0200

    4.0

--- scripts/script.js
+++ scripts/script.js
@@ -5,82 +5,52 @@ function Book(title, author, status, id) {
     this.author = author;
     this.status = status;
     this.id = id;
-
 }
 
 //event listener on submit to fire up addBook()
 const submitButton = document.getElementById("submit-button");
-console.log(submitButton);
 submitButton.addEventListener("click", addBook);
 
-//function to add a new book when submit is clicked
+//defining the bookList
+let bookList = Object.entries(localStorage);
+
 function addBook(e) {
     //getting values from inputs
     let title = document.getElementById("book-title").value;
     let author = document.getElementById("author").value;
     let status = document.getElementById("read-unread").value;
 
-    //using the constructor to add a new Book in localstorage
-    let newBook = new Book(title, author, status, localStorage.length);
-    localStorage.setItem(localStorage.length, JSON.stringify(newBook));
+    //maxID is used to generate new IDs to not have repeating IDs
+    let maxID = 0;
+    // [0, 0, 0] is used to avoid a bug that sets a key to -infinity
+    let idArray = [0, 0, 0];
+    bookList.forEach((element, index) => {
+        console.log(element[0]);
+        idArray.push(element[0]);
+    });
+    console.log("idArray:");
+    console.log(idArray);
+    maxID = Math.max(...idArray);
+    console.log("max id is now: " + maxID);
+
+    //setting the id that should be a larger number than maxID
+    let id = maxID + 1;
+    console.log(id);
+
+    //constructing a new Book and adding it in localstorage
+    let newBook = new Book(title, author, status, id);
+    console.log("newBook:");
+    console.log(newBook);
+    localStorage.setItem(id, JSON.stringify(newBook));
 
     //reloads the page so that all books can be loaded on the page
     window.location.reload();
-
-}
-
-//iterating through all books that are stored in localstorage
-for (let i=0; i<localStorage.length; i++) {
-
-    //getting values from props in localstorage
-    let currentBook = JSON.parse(localStorage[i]);
-    let title = currentBook.title;
-    let author = currentBook.author;
-    let status = currentBook.status;
-    let id = currentBook.id;
-    console.log(currentBook);
-
-    //creating html elements for the books
-    let bookE = document.createElement("div");
-    bookE.classList.add("book");
-    bookE.setAttribute("id", "book-"+id);
-    let bookListE = document.getElementById("book-list");
-    bookListE.appendChild(bookE);
-
-    //adding containers for each book detail
-    //title, author, status, delete
-    let bookTitleContainerE = document.createElement("div");
-    let bookAuthorContainerE = document.createElement("div");
-    let bookStatusContainerE = document.createElement("div");
-    let bookDeleteContainerE = document.createElement("div");
-
-    //adding classes for css styling on containers
-    bookTitleContainerE.classList.add("book-detail");
-    bookAuthorContainerE.classList.add("book-detail");
-    bookStatusContainerE.classList.add("book-detail");
-    bookDeleteContainerE.classList.add("book-detail");
-
-    //adding the containers on the page
-    bookE.appendChild(bookTitleContainerE);
-    bookE.appendChild(bookAuthorContainerE);
-    bookE.appendChild(bookStatusContainerE);
-    bookE.appendChild(bookDeleteContainerE);
-
-    //adding title, author, status, delete in the containers
-    let bookTitleE = document.createElement("p");
-    bookTitleE.innerHTML = title;
-    let bookAuthorE = document.createElement("p");
-    bookAuthorE.innerHTML = author;
-    let bookStatusE = document.createElement("button");
-    bookStatusE.innerHTML = status;
-    let bookDeleteE = document.createElement("button");
-    bookDeleteE.innerHTML = "delete"
-    bookTitleContainerE.appendChild(bookTitleE);
-    bookAuthorContainerE.appendChild(bookAuthorE);
-    bookStatusContainerE.appendChild(bookStatusE);
-    bookDeleteContainerE.appendChild(bookDeleteE);
     
-
-
-
 }
+
+bookList.forEach((element, index) => {
+    console.log("index");
+    console.log(index);
+    console.log("element");
+    console.log(element);
+});
